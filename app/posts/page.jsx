2a5d49dc8a1dfe7e1d06edf64c614fe360d2fd44
dakrushel <151683371+dakrushel@users.noreply.github.com>
2024-11-23 15:38:17 -0700

    Update page.jsx

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -5,62 +5,6 @@ import PostList from "../components/PostList";
 import { useAuth } from "../context/AuthContext";
 import { useRouter } from "next/navigation";
 
-// a component that when click will open a modal to add a post to my page
-// another component to retrieve all posts from the database
-
-<<<<<<< Updated upstream
- async function getData() {
-  try {
-    const res = await fetch("http://localhost:3000/api/posts", {cache: "no-store"});
-    if(!res.ok) {
-      console.error("API error: ", res.status, res.statusText);
-      return [];
-    }
-      return res.json();
-  } catch (error) {
-    console.error("Fetch failed: ", error.message);
-    return [];
-  }
-}
-const page = async () => {
-   let posts = [];
-
-   try {
-    posts = await getData();
-   } catch (error) {
-    console.error("Error fetching posts: ", error.message);
-   }
-
-  return (
-    <main className='flex flex-col justify-between p-24'>
-        <h1 className='p-5'>My Movies App</h1>
-        <div>
-            <AddPost />
-            <PostList posts={posts} />
-        </div>
-=======
-//  async function getData() {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/posts", {cache: "no-store"});
-//     if(!res.ok) {
-//       console.error("API error: ", res.status, res.statusText);
-//       return [];
-//     }
-//       return res.json();
-//   } catch (error) {
-//     console.error("Fetch failed: ", error.message);
-//     return [];
-//   }
-// }
-// const page = async () => {
-//    let posts = [];
-
-//    try {
-//     posts = await getData();
-//    } catch (error) {
-//     console.error("Error fetching posts: ", error.message);
-//    }
-
 const Page = () => {
   const { user } = useAuth();
   const router = useRouter();
@@ -107,7 +51,6 @@ const Page = () => {
         <AddPost />
         <PostList posts={posts} />
       </div>
->>>>>>> Stashed changes
     </main>
   );
 };
