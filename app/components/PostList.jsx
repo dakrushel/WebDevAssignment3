3d33855c0c53d3700c9ebee6501d031cd0a19616
dakrushel <151683371+dakrushel@users.noreply.github.com>
2024-11-23 15:24:35 -0700

    Update PostList.jsx

--- app/components/PostList.jsx
+++ app/components/PostList.jsx
@@ -1,24 +1,11 @@
-<<<<<<< Updated upstream
 import React from 'react'
 import Post from './Post'
-// const PostList = ({post}) => {
   const PostList = ({posts = [] }) => {
   return (
     <ul className='p-5'>
         {posts.map((post) => (
          <Post key={post.id} post={post} />
         ))}
-=======
-import React from "react";
-import Post from "./Post";
-
-const PostList = ({ posts = [] }) => {
-  return (
-    <ul className="p-5">
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
->>>>>>> Stashed changes
     </ul>
   );
 };
