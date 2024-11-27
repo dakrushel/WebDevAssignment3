1438b09f1ddf8d0a3c700850c188d1ca1753a49d
dakrushel <151683371+dakrushel@users.noreply.github.com>
2024-11-27 10:48:51 -0700

    Update Modal.jsx

--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ children, showModal, setShowModal }) => {
   return (
     showModal && (
       <div className="fixed inset-0 bg-black/50 flex justify-center items-center">
-        <div className="bg-blue-900 text-red-500 rounded-lg shadow-lg w-1/2 p-6">
+        <div className="bg-blue-900 text-white rounded-lg shadow-lg w-1/2 p-6">
             <button
               className="text-2xl mb-3"
               onClick={() => setShowModal(false)}
